3496275d6c24a7126ce6fea216dc3c8f83f16374
Krzysztof Piatkowski <krzysztofpiatkowski1987@gmail.com>
2025-05-17 10:23:28 +0200

    Poprawa ostylowania podczas hover i zamiana <a> na <Link> w komponencie FeatureBox

--- src/components/common/FeatureBox/FeatureBox.js
+++ src/components/common/FeatureBox/FeatureBox.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styles from './FeatureBox.module.scss';
 
 const FeatureBox = ({ icon, children }) => (
-  <a href='#' className={styles.root}>
+  <Link to='#' className={styles.root}>
     {icon && (
       <div className={styles.iconWrapper}>
         <FontAwesomeIcon className={styles.icon} icon={icon} />
       </div>
     )}
     <div className={styles.content}>{children}</div>
-  </a>
+  </Link>
 );
 
 FeatureBox.propTypes = {
