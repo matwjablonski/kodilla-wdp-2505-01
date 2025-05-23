6a53443275077ac4b66d7b05730b2abca07aeac1
Paulina Partyka <paulinapartyka2@gmail.com>
2025-05-23 17:30:41 +0200

    Dodano funkcję oznaczania produktów do ulubionych z wykorzystaniem redux

--- src/redux/productsRedux.js
+++ src/redux/productsRedux.js
@@ -5,9 +5,21 @@ export const getCount = ({ products }) => products.length;
 export const getNew = ({ products }) =>
   products.filter(item => item.newFurniture === true);
 
+const TOGGLE_FAVORITE = 'products/TOGGLE_FAVORITE'
+
+export const toggleFavorite = (id) => ({
+  type: TOGGLE_FAVORITE,
+  payload: id,
+});
+
 /* reducer */
 export default function reducer(statePart = [], action = {}) {
   switch (action.type) {
+    case TOGGLE_FAVORITE:
+      console.log('toggle favorite for id:', action.payload);
+      return statePart.map(product=>
+        product.id===action.payload ? {...product, favorite: !product.favorite } : product
+      );
     default:
       return statePart;
   }
