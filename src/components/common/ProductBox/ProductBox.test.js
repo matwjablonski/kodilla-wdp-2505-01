fc8c28e78a61d5568a2d235b198f87504a121135
Saif Al-Diresee <saif.aldiresee@gmail.com>
2025-05-29 22:19:45 +0200

    Resolve merge conflict + add to favorite and image support

--- src/components/common/ProductBox/ProductBox.test.js
+++ src/components/common/ProductBox/ProductBox.test.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import ProductBox from './ProductBox';
 
+
 describe('Component ProductBox', () => {
   it('should render without crashing', () => {
     const component = shallow(<ProductBox />);
